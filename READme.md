fc4dda33cbeff99aed35b2798688e649499639fa
mvanpractice <michaelmanggay@gmail.com>
2025-06-13 09:16:20 +0800

    final answer to TOP first DOM exercise

---
+++ READme.md
@@ -0,0 +1,8 @@
+# In the first "first-TOP-DOM" exercise, I commented out my first solution since I noticed that I am using a lot of redundancy. 
+# I used ChatGPT and it gave a nice answer using Object.assign
+# I liked it and practiced doing what GPT gave without looking but I still ended up looking since my code didn't run. 
+# The mistake was, in the second if statement, I use options.type instead of options.style 
+# I realized that I should have indeed used options.style since I am basically checking if this object property exists in the options object
+# ChatGPT used shorthand ifs but since I am not yet comfortable with those syntaxes, I used what I am learning in TOP as of now which is the long if with braces.
+# I wish I am smart and I will not forget this. I work 9-5 and only occationally follow TOP to hopefully learn web dev basics. 
+# If ever I somehow achieve this in the future, I hope I can go back and read this to further motivate myself that although I am dull and slow, that I fill eventually and finally achieve it!
--- first-TOP-DOM.js
+++ first-TOP-DOM.js
@@ -1,30 +1,79 @@
 
 const container = document.querySelector('.container');
 
-const p = document.createElement('p');
-const h3 = document.createElement('h3');
-const div = document.createElement('div');
+// const p = document.createElement('p');
+// const h3 = document.createElement('h3');
+// const div = document.createElement('div');
 
-const h1 = document.createElement('h1');
-const p2 = document.createElement('p');
+// const h1 = document.createElement('h1');
+// const p2 = document.createElement('p');
 
-p.style.color = 'red';
-p.textContent = 'Hey I am red!';
+// p.style.color = 'red';
+// p.textContent = 'Hey I am red!';
 
-h3.style.color = 'blue';
-h3.textContent = "Hey I'm blue h3";
+// h3.style.color = 'blue';
+// h3.textContent = "Hey I'm blue h3";
 
-div.style.backgroundColor = 'pink';
-div.style.border = '1px solid black';
+// div.style.backgroundColor = 'pink';
+// div.style.border = '1px solid black';
 
-h1.textContent = "I'm in a div";
-p2.textContent = 'ME TOO!';
+// h1.textContent = "I'm in a div";
+// p2.textContent = 'ME TOO!';
 
-container.appendChild(p);
-container.appendChild(h3);
+// container.appendChild(p);
+// container.appendChild(h3);
 
-div.appendChild(h1);
-div.appendChild(p2);
+// div.appendChild(h1);
+// div.appendChild(p2);
 
-container.append(div);
+// container.append(div);
 
+function createElement(type, options = {}) {
+
+    const domElement = document.createElement(type);
+
+    if (options.text) {
+        domElement.textContent = options.text;
+    }
+
+    if (options.style) {
+        Object.assign(domElement.style, options.style);
+    }
+
+    return domElement;
+
+}
+
+
+const p = createElement('p', {
+    text: 'Hey, I am red!',
+    style: {
+        color: 'red'
+    }
+});
+
+const h3 = createElement('h3', {
+    text: "I'm a blue h3",
+    style: {
+        color: 'blue'
+    }
+});
+
+const div = createElement('div', {
+    style: {
+        backgroundColor: 'pink',
+        border: '1px solid black'
+    }
+});
+
+const h1 = createElement('h1', {
+    text: 'I am in a div',
+});
+
+const p2 = createElement('p', {
+    text: 'ME TOO!',
+});
+
+div.append(h1, p2);
+
+container.append(p, h3, div);
